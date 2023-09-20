68c33b9dcd33c3446a7b38c47ea6ad0026a48601
hatahalu <123358897+hatahalu@users.noreply.github.com>
2023-09-20 15:02:12 +0900

    Update Serial_sent_one.py

--- Serial_sent_one.py
+++ Serial_sent_one.py
@@ -1,20 +1,19 @@
 import serial
-import RPi.GPIO as GPIO
-import time
 
-TX_PIN = 14
-
-ser = serial.Serial('/dev/serial0',9600)
-
-ser.timeout = 1
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(TX_PIN,GPIO.OUT)
+# シリアルポートの設定
+ser = serial.Serial('/dev/ttyS0', 9600)  # ポートとボーレートを適切に設定してください
 
 try:
-    while True:
-        ser.write("1")
-        time.sleep(1)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    ser.close()
+    # "1" を送信
+    ser.write(b'1')
+    print("送信: 1")
+    
+    # 必要に応じて受信処理を追加できます
+    # received_data = ser.read(1)
+    # print(f"受信: {received_data.decode('utf-8')}")
+
+except Exception as e:
+    print(f"エラー: {str(e)}")
+
+finally:
+    ser.close()  # シリアルポートを閉じる
