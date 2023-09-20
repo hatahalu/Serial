6a9f50494886ef6c2c7032eac4f9ee72acdd9662
hatahalu <123358897+hatahalu@users.noreply.github.com>
2023-09-20 16:08:58 +0900

    Update Serial_sent_one.py

--- Serial_sent_one.py
+++ Serial_sent_one.py
@@ -1,19 +1,9 @@
 import serial
-
-# シリアルポートの設定
-ser = serial.Serial('/dev/ttyS0', 9600)  # ポートとボーレートを適切に設定してください
-
+import time
+ser = serial.Serial('/dev/ttyS0',9600)
 try:
-    # "1" を送信
-    ser.write(b'1')
-    print("送信: 1")
-    
-    # 必要に応じて受信処理を追加できます
-    # received_data = ser.read(1)
-    # print(f"受信: {received_data.decode('utf-8')}")
-
-except Exception as e:
-    print(f"エラー: {str(e)}")
-
-finally:
-    ser.close()  # シリアルポートを閉じる
+    while True:
+        ser.write(b'1')
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    ser.close()
